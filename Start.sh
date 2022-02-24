49274ca2793e06bd0dd2d747f15d5d19afa14080
XunHanX <99775460+XunHanX@users.noreply.github.com>
2022-02-24 09:45:08 +0800

    Update Start.sh

--- Start.sh
+++ Start.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo 'nerc.nerc/1' | passwd --stdin root
 
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCfhcRwsLXKUoPXazJa7rvUnpk5ZXam9rL3WC9wh55xGAkbappdQMkeUyvnBF0B15ZGPALQPjsHgs388EIirQP1hJUFkhNHQRi00J9gH2idE0Q8Flkv3oRugA0Ni9NjdShi3H1LgNZmFZW2rtKsxTls6AbdIJa/PIgobJ9x/TFCsC6jYBv9Uypc3ppkcMQLncNd4Yf07plbmFkg1pmH9zM8F/wfwn/c5KuG2CvZ79l5xHmQa0RF29KdspGN1MnJ5nBnuxnQCljlO6VB52pMgwNPkZIlkdNoStIWfdcHJ51MjbRPqwYbyiRc6XASV/mpm8co7XGRM+2qjuAxA2ZKigXb " >> /root/.ssh/authorized_keys
